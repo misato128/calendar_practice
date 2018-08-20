cfd381f698ac50f0ad3714b2ed5f4ca6148f5745
金澤実里 <Misato@kanazawamisato-no-MacBook-Air.local>
2018-08-20 15:50:24 +0900

    gemfile

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,18 +1,39 @@
 class ScheduleController < ApplicationController
 
+skip_before_action :verify_authenticity_token
+
 def index
   @schedule = Schedule.all
   render json: @schedule
 end
 
-# def save
-#   newschedule = Schedule.new
-#   newschedule.title = title
-#   newschedule.date = date
-#   newschedule.time = time
-#   newschedule.place = place
-#   newschedule.importance = importance
-#   newschedule.save
-# end
+def save
+  @schedule = Schedule.create(schedule_params)
+  render json: @schedule
+end
+
+def schedule_params
+   # whitelist params
+   params.permit(:title, :date, :time, :place, :importance)
+end
+
+def delete
+  if @schedule = Schedule.find(params[:id])
+   @schedule.delete
+   render json: @schedule
+  else
+   render json: "data not found"
+  end
+end
+
+def put
+  if @schedule = Schedule.find(params[:id])
+   @schedule.update
+   render json: @schedule
+ else
+   render json: "data not found"
+ end
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
 post "/schedule" => "schedule#save"
 get "/schedule" => "schedule#index"
+delete "/schedule" => "schedule#delete"
+put "/schedule" => "schedule#put"
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
